32205d5e3235b3c0c06d5a8928d8ea9c81507e00
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-02-18 16:59:50 +0500

    renovation

--- init.lua
+++ init.lua
@@ -1,76 +1,76 @@
 local s = core.get_mod_storage()
-local function invis_on(player)
-    player:set_properties({
-        visual = "",
-        --collisionbox = {-0.01, 0, -0.01, 0.01, 0, 0.01},
-        show_on_minimap = false,
-        pointable = false,
-    })
-    player:set_nametag_attributes({color={a=0},text = " "})
+vanish = {}
+vanish.vanished = {}
+vanish.on = function(player)
+	vanish.vanished[player:get_player_name()] = true
+	player:set_properties({
+		visual = "",
+		--collisionbox = {-0.01, 0, -0.01, 0.01, 0, 0.01},
+		show_on_minimap = false,
+		pointable = false,
+	})
+	player:set_nametag_attributes({color={a=0},text = " "})
 end
-local function invis_off(player)
-    local name = player:get_player_name()
-    if not name then return end
-    player:set_properties({
-		     	visual = "mesh",
-     			--collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
-		     	show_on_minimap = true,
-	     		pointable = true,
+vanish.off = function(player)
+	vanish.vanished[player:get_player_name()] = nil
+	local name = player:get_player_name()
+	if not name then return end
+	player:set_properties({
+			 	visual = "mesh",
+	 			--collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
+			 	show_on_minimap = true,
+		 		pointable = true,
   		})
-    if core.get_modpath("ranks") then
-        local rank,color = ranks.get_rank(name)
-        if rank and color then
-            player:set_nametag_attributes({color={a=255,r=255,g=255,b=255},text = core.colorize(color,rank).." "..name})
-        else
-            player:set_nametag_attributes({color={a=255,r=255,g=255,b=255},text = name})
-        end
-    else
-        player:set_nametag_attributes({color={a=255,r=255,g=255,b=255},text = name})
-    end
+	if core.get_modpath("ranks") then
+		ranks.update_ntag(name)
+	else
+		player:set_nametag_attributes({color={a=255,r=255,g=255,b=255},text = name})
+	end
 end
+
 core.register_privilege("vanish","Allows to make players invisible")
 core.register_on_joinplayer(function(player)
-    local name = player:get_player_name()
-    if not name then return end
-    local isinvis = s:get_string(name)
-    if isinvis == "1" then
-        core.after(0.1,function()
-            invis_on(player)
-        end)
-    end
+	local name = player:get_player_name()
+	if not name then return end
+	local isinvis = s:get_string(name)
+	if isinvis == "1" then
+		core.after(0.1,function()
+			vanish.on(player)
+		end)
+	end
 end)
 core.register_chatcommand("vanish",{
-    description = "Toggle invisibility of player",
-    privs = {vanish=true},
-    params = "<name>",
-    func = function(name,param)
-        if param == "" then param = name end
-        local player = core.get_player_by_name(param)
-        local isinvis = s:get_string(param)
-        if isinvis == "1" then
-            s:set_string(param,"")
-            if player then
-                invis_off(player)
-            end
-            return true, "-!- "..param.." unvanished"
-        else
-            s:set_string(param,"1")
-            if player then
-                invis_on(player)
-            end
-            return true, "-!- "..param.." vanished"
-        end
+	description = "Toggle invisibility of player",
+	privs = {vanish=true},
+	params = "<name>",
+	func = function(name,param)
+		if param == "" then param = name end
+		local player = core.get_player_by_name(param)
+		local isinvis = s:get_string(param)
+		if isinvis == "1" then
+			s:set_string(param,"")
+			if player then
+				vanish.off(player)
+			end
+			return true, "-!- "..param.." unvanished"
+		else
+			s:set_string(param,"1")
+			if player then
+				vanish.on(player)
+			end
+			return true, "-!- "..param.." vanished"
+		end
 end})
-      
+
 core.register_chatcommand("vanished",{
-    description = "Show list of vanished players",
-    privs = {vanish=true},
-    func = function(name,param)
-        local out = {}
-        local tabl = s:to_table().fields
-        for nick,val in pairs(tabl) do
-            table.insert(out,nick)
-        end
+	description = "Show list of vanished players",
+	privs = {vanish=true},
+	func = function(name,param)
+		local out = {}
+		local tabl = s:to_table().fields
+		for nick,val in pairs(tabl) do
+			table.insert(out,nick)
+		end
 	table.sort(out)
-        return true, "Vanished: "..table.concat(out,", ")
+		return true, "Vanished: "..table.concat(out,", ")
 end})
